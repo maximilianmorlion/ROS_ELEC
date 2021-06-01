9ff4d5eb096aa56bf3ec8586cbb737f0556956e5
maximilianmorlion <43882546+maximilianmorlion@users.noreply.github.com>
2021-06-01 18:49:48 +0200

    Add files via upload

--- launch/waiter_launch.py
+++ launch/waiter_launch.py
@@ -1,18 +1,25 @@
-# from launch import LaunchDescription
-# from launch_ros.actions import Node
-# 
-# def generate_launch_description():
-#     return LaunchDescription([
-#         Node(
-#             package='turtlesim',
-#             namespace='turtlesim1',
-#             executable='turtlesim_node',
-#             name='sim'
-#         )
-#     ])
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld= LaunchDescription()
+        paella_servnode = Node(
+            package='waiter_bringup',
+            executable='paella',
+            name='paella_server'
+        )
+        paella_clientnode = Node(
+            package='waiter_bringup',
+            executable='paella_client',
+            name='paella_server'
+        )
+    ld.add_action(paella_servnode)
+    ld.add_action(paella_clientnode)
+    return ld
+
+import os
 
 # trop cheeater donc à potentiellement changer pas la méthode demandée
-import os
 os.popen("export TURTLEBOT3_MODEL=waffle")
 os.popen("export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/foxy/share/turtlebot3_gazebo/models")
 os.popen("ros2 launch nav2_bringup tb3_simulation_launch.py")
@@ -22,9 +29,12 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
+
 publisher = Node.create_publisher(PoseWithCovarianceStamped, 'initialpose', 10)
 msgPose = PoseWithCovarianceStamped()
 msgPose.pose.pose.position.x = 2
 msgPose.pose.pose.position.y = 0
 
 publisher.publish(msgPose)
+
+
